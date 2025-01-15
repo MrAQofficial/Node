5986f055776c0f441b16f092da3a0eb0ac6b4ef7
Aqib Hassan <work.aqibhassan@gmail.com>
2025-01-16 01:35:19 +0500

    last 15 Jan

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Navbar from './Navbar';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
     <Navbar />
+    <Routes>
+      <Route path="/" element={<h1>products</h1>}  />
+      <Route path="/add" element={<h1>Add product</h1>}  />
+      <Route path="/update" element={<h1>Update product</h1>}  />
+      <Route path="/logOut" element={<h1>Logout</h1>}  />
+      <Route path="/profile" element={<h1>Profile</h1>}  />
+    </Routes>
     </BrowserRouter>
     
   );
--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -9,25 +9,31 @@ const Navbar = () => {
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <Link class="navbar-brand" to="#">Navbar w/ text</Link>
+                <Link class="navbar-brand" to="/">E-com</Link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarText">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <Link class="nav-link" to="#">Home <span class="sr-only">(current)</span></Link>
+                            <Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
                         </li>
                         <li class="nav-item">
-                            <Link class="nav-link" to="#">Features</Link>
+                            <Link class="nav-link" to="add">Add Product</Link>
                         </li>
                         <li class="nav-item">
-                            <Link class="nav-link" to="#">Pricing</Link>
+                            <Link class="nav-link" to="update">Update Product</Link>
                         </li>
                     </ul>
-                    <span class="navbar-text">
-                        Navbar text with an inline element
-                    </span>
+                    <ul class="navbar-nav ">
+                        <li class="nav-item">
+                            <Link class="nav-link" to="logOut">Logout</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="profile">Profile</Link>
+                        </li>
+                    </ul>
+
                 </div>
             </nav>
         </>
