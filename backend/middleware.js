8b58db12823f5ae73b4a253ea9afa86393ca52ae
Aqib Hassan <138342869+MrAQofficial@users.noreply.github.com>
2025-01-16 07:56:40 +0500

    Update middleware.js
    pre-corrections

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,11 +1,11 @@
 // MiddleWare Here
 const ageFilter = (req, res, next) => {
     if (!req.query.age) {
-        // res.render('ageError')
-        next()
+        res.render('ageError')
+        // next()
     } else if (req.query.age < 18) {
-        // res.render('underAge')
-        next()
+        res.render('underAge')
+        // next()
     } else {
         next();
     }
